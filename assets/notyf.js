87c3cc6761ea65147e17dc41742f658a5520c84d
mrstph <118338282+mrstph@users.noreply.github.com>
2023-03-04 21:07:19 +0100

    Add layouts components and twig templates
    - base and auth and base layout
    - components (navbar + sidebar + messages)
    - notyf for toats messages

---
+++ assets/notyf.js
@@ -0,0 +1,26 @@
+import { Notyf } from 'notyf';
+import 'notyf/notyf.min.css'; // for React, Vue and Svelte
+
+// Create an instance of Notyf
+const notyf = new Notyf({
+    duration:5000,
+    position: {
+        x: 'right',
+        y: 'bottom'
+    },
+    dismissible: true,
+    ripple: false
+});
+
+let messages = document.querySelectorAll('div');
+console.log(messages)
+
+messages.forEach(message => {
+    if(message.className == 'notify success-message'){
+        notyf.success(message.innerHTML);
+    }
+
+    if(message.className == 'notify error-message'){
+        notyf.error(message.innerHTML);
+    }
+});
