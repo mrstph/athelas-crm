80a7bfdb0457ddb82cc2bd3f1bceab9210954e72
mrstph <118338282+mrstph@users.noreply.github.com>
2023-03-09 11:53:54 +0100

    update contact repository

--- src/Repository/ContactRepository.php
+++ src/Repository/ContactRepository.php
@@ -2,6 +2,7 @@
 
 namespace App\Repository;
 
+use App\Entity\User;
 use App\Entity\Contact;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Persistence\ManagerRegistry;
@@ -38,6 +39,57 @@ class ContactRepository extends ServiceEntityRepository
             $this->getEntityManager()->flush();
         }
     }
+
+    // public function findAllContactsThatAreNotUsers()
+    // {
+        // $conn = $this->getEntityManager()->getConnection();
+
+        // return $this->convertResult(
+        //     $conn
+        //         ->prepare("select * from contacts as c WHERE id NOT IN (select contact_id from users)")
+        //         ->executeQuery()
+        //         ->fetchAllAssociative()
+        // );
+
+        // $qb = $this->createQueryBuilder('c');
+        
+        // $qb->where($qb->expr()->notIn('c.id', 
+        //     $this->getEntityManager()->createQueryBuilder()
+        //         ->select('u.contact')
+        //         ->from('App\Entity\User', 'u')
+        //         ->getDQL()
+        // ));
+        
+        // return $qb->getQuery()->getResult();
+    // }
+
+    //
+    // public function convertResult(array $results)
+    // {
+    //     if (empty($results)){
+    //         return;
+    //     }
+
+    //     $contacts = [];
+
+    //     foreach ($results as $res) {
+    //         $contact = new Contact();
+
+    //         //miss setID & company due to being foreign key
+    //         $contact->setFirstname($res['firstname']);
+    //         $contact->setLastname($res['lastname']);
+    //         $contact->setEmail($res['email']);
+    //         $contact->setPhone($res['phone']);
+    //         $contact->setAddress($res['zip_code']);
+    //         $contact->setCity($res['city']);
+    //         $contact->setCountry($res['country']);
+    //         $contact->setJobPosition($res['job_position']);
+
+    //         array_push($contacts, $contact);
+
+    //         return $contacts;
+    //     }
+    // }
 
 //    /**
 //     * @return Contact[] Returns an array of Contact objects
