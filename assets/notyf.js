2ef3ef2bf010ce937052e4496c69c2511befe031
mrstph <118338282+mrstph@users.noreply.github.com>
2023-03-09 11:48:15 +0100

    configure messages, sidebar + navbar, and error pages
    - add notyf js for toasts messages
    - update sidebar
    - update navbar
    - update messages
    - update all error pages

--- assets/notyf.js
+++ assets/notyf.js
@@ -1,5 +1,5 @@
 import { Notyf } from 'notyf';
-import 'notyf/notyf.min.css'; // for React, Vue and Svelte
+// import 'notyf/notyf.min.css'; // for React, Vue and Svelte
 
 // Create an instance of Notyf
 const notyf = new Notyf({
