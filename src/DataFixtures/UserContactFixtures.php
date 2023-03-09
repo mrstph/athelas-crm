fa95cfb23caa4b58374a86863f6cd9393376cc10
mrstph <118338282+mrstph@users.noreply.github.com>
2023-03-09 11:46:07 +0100

    move routes to yaml file, add fixtures

---
+++ src/DataFixtures/UserContactFixtures.php
@@ -0,0 +1,91 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Contact;
+use App\Entity\User;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+
+class UserContactFixtures extends Fixture
+{
+
+    private UserPasswordHasherInterface $hasher;
+
+    public function __construct(UserPasswordHasherInterface $hasher)
+    {
+        $this->hasher = $hasher;
+    }
+
+    public function load(ObjectManager $manager): void
+    {
+
+        // ~~~~~~~~~~~~~~
+        // ~~~ ADMIN  ~~~
+        // ~~~~~~~~~~~~~~
+
+        $contact = new Contact();
+
+        $contact->setFirstname('admin');
+        $contact->setLastname('athelas');
+        $contact->setEmail('admin@athelas.fr');
+        $contact->setPhone('+33567890987');
+        $contact->setAddress('1 rue du château d\'eau');
+        $contact->setZipCode('67000');
+        $contact->setCity('Strasbourg');
+        $contact->setCountry('France');
+        $contact->setJobPosition('Administrateur');
+        // $contact->setCompany('Athelas');
+        $contact->setCreatedAt(new \DateTimeImmutable());
+
+        $manager->persist($contact);
+
+        $user = new User();
+        $user->setUsername('admin');
+        $user->setRoles(['ROLE_ADMIN']);
+        $password = $this->hasher->hashPassword($user,'pass_1234');
+        $user->setPassword($password);
+        $user->setConsent(true);
+        $user->setPositionCompany('Administrateur');
+        $user->setPicture('placeholder');
+        $user->setContact($contact);
+
+        $manager->persist($user);
+
+        $manager->flush();
+
+        // ~~~~~~~~~~~~~~~~~~~~~~~~
+        // ~~~ ANOTHER EMPLOYEE ~~~
+        // ~~~~~~~~~~~~~~~~~~~~~~~~
+
+        $contact = new Contact();
+
+        $contact->setFirstname('Tamara');
+        $contact->setLastname('Smith');
+        $contact->setEmail('tamara.smith@athelas.fr');
+        $contact->setPhone('+336878489526');
+        $contact->setAddress('3 avenue de la forêt');
+        $contact->setZipCode('67000');
+        $contact->setCity('Strasbourg');
+        $contact->setCountry('France');
+        $contact->setJobPosition('Collaborateur');
+        $contact->setCreatedAt(new \DateTimeImmutable());
+
+        $manager->persist($contact);
+
+        $user = new User();
+        $user->setUsername('tsmith');
+        $user->setRoles(['ROLE_USER']);
+        $password = $this->hasher->hashPassword($user,'pass_1234');
+        $user->setPassword($password);
+        $user->setConsent(true);
+        $user->setPositionCompany('Directrice Marketing');
+        $user->setPicture('placeholder');
+        $user->setContact($contact);
+
+        $manager->persist($user);
+
+        $manager->flush();
+    }
+}
